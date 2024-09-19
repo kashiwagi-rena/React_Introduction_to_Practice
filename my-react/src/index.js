7f58951c706a786211b9af1525e97baef0810a24
kashiwagi-rena <azisai07@gmail.com>
2024-09-19 20:35:55 +0900

    3-1-2 Propsの基本

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyHello from './chap03/MyHello';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(() => {
   root.render(
-      <p>現在時刻：{(new Date()).toLocaleString()}</p>
+    <MyHello myName="鈴木" />
     );
-}, 1000);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
