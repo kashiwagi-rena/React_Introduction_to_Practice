92126a92f5275ddd2f6cc98420a8ff5085fa4d2c
kashiwagi-rena <azisai07@gmail.com>
2024-09-22 01:34:49 +0900

    childrenから目的の要素を取り出す

--- my-react/src/index.js
+++ my-react/src/index.js
@@ -6,10 +6,11 @@ import TitledPanel from './chap03/TitledPanel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(() => {
-  const title= <p>メンバー募集中！</p>;
-  const body=<p>ようこそ、WINGSプロジェクトへ！！</p>;
   root.render(
-      <TitledPanel title={title} body={body}></TitledPanel>
+      <TitledPanel>
+        <p key="title">メンバー募集中！</p>
+        <p key="body">ようこそ、WINGSプロジェクトへ！！</p>
+      </TitledPanel>
   );
 });
 
