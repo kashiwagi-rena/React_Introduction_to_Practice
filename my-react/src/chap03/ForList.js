b2c7ff7648d5cb7b9b07919a944af2b4ea719747
kashiwagi-rena <azisai07@gmail.com>
2024-09-21 15:48:18 +0900

    リスト項目を特定するkey属性

--- my-react/src/chap03/ForList.js
+++ my-react/src/chap03/ForList.js
@@ -5,14 +5,14 @@ export default function ForList({ src }) {
     <dl>
       {
         src.map(elem => (
-          <>
+          <React.Fragment key={elem.isbn}>
             <dt>
               <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
                 {elem.title} ({elem.price}円)
               </a>
             </dt>
             <dd>{elem.summary}</dd>
-          </>
+          </React.Fragment>
         ))}
     </dl>
   );
